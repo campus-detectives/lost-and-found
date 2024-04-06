5a5026b51feecaa4e38043e29975b966347c7584
Snehil Chatterjee <snehilchatterjee7924@gmail.com>
2024-04-06 14:54:36 +0530

    Proposed solution page

---
+++ lostfound/App_Backup.js
@@ -0,0 +1,107 @@
+import React from "react";
+import {
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  StyleSheet,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+  Button,
+  Keyboard,
+  StatusBar,
+  Image,
+  onChangeText,
+  Pressable,
+} from "react-native";
+
+export default function Signup() {
+  const username = "";
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <StatusBar backgroundColor="#5d2d33" />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0b0b18",
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: "space-around",
+  },
+  header: {
+    fontSize: 36,
+    marginBottom: 48,
+  },
+  textInput: {
+    height: 40,
+    borderColor: "#000000",
+    borderBottomWidth: 1,
+    marginBottom: 36,
+  },
+  btnContainer: {
+    backgroundColor: "white",
+    marginTop: 12,
+  },
+
+  logo: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderColor: "black",
+    borderWidth: 2,
+  },
+
+  center: {
+    marginTop: "0%",
+    alignItems: "center",
+    backgroundColor: "#0b0b18",
+  },
+
+  h1: {
+    paddingTop: 10,
+    alignItems: "center",
+  },
+
+  input: {
+    marginBottom: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#262626",
+    color: "white",
+  },
+  lable: {
+    fontSize: 18,
+    color: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingRight: 10,
+    fontWeight: "bold",
+    marginBottom: 2,
+  },
+  button1: {
+    width: 270,
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#ffbf00",
+    marginTop: 12,
+    justifyContent: "center",
+  },
+});
